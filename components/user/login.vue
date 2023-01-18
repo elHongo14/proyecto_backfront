46d1b70a2db9f310f13f1b1507dd7f74b3e7ca3f
elHongo14 <elhongo1409@outlook.com>
2023-01-18 10:19:30 -0600

    Implementamos login cards

---
+++ components/user/login.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card class="cardLogin">
+        <v-card-title>Login</v-card-title>
+        <v-card-text>
+            <v-form v-model="formularioValido">
+                <v-text-field label="Correo Electrónico"
+                                placeholder="Correo Electrónico"
+                                v-model="correoElec"
+                />
+            </v-form>
+        </v-card-text>
+        <v-card-actions>
+            Boton pa culiar
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            formularioValido: false,
+            correoElec: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+.cardLogin {
+    background-color: #7be396;
+    border-radius: 10px;
+    width: 500px;
+    height: 500px;
+}
+</style>
---
+++ layouts/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-row class="renglon">
+        <nuxt />
+    </v-row>
+</template>
+
+<style>
+.renglon {
+    width: 100vw;
+    height: 1000vh;
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+}
+</style>
